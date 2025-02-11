327a0af540430c224d3386427dd177f5a3d2bda2
Karimtarek011420 <karimtarek011420@gmail.com>
2025-02-11 23:52:48 +0200

    uiddi

--- src/app/_Compontents/CountryHeader/CountryHeader.jsx
+++ src/app/_Compontents/CountryHeader/CountryHeader.jsx
@@ -39,7 +39,7 @@ export default async function CountryHeader() {
   return (
     <div className="countryheader position-relative py-5">
       {/* قائمة الروابط */}
-      <div className="position-absolute country-list w-100">
+      <div className="position-absolute country-listbegin w-100">
         <ul className="list-unstyled d-flex justify-content-center align-items-center">
           <li
             className="country-list-links bg-white mx-2"
