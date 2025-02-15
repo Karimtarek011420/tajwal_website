de0814013fac92677586126f7485c2ad2398d069
Karimtarek011420 <karimtarek011420@gmail.com>
2025-02-16 01:00:12 +0200

    uiddi

--- src/app/(auth)/layout.jsx
+++ src/app/(auth)/layout.jsx
@@ -1,10 +1,8 @@
-// import { Toaster } from "react-hot-toast";
 
 export default function RootLayout({ children }) {
   return (
     <main style={{backgroundColor:'#F9F9F9' , minHeight:'100vh'}}>
       <div>
-        {/* <Toaster/> */}
         {children}
         </div>
     </main>
