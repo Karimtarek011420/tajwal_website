43a25bfd8a554142e976d2ea5a562b7fc5cbc867
Karimtarek011420 <karimtarek011420@gmail.com>
2025-02-17 03:45:55 +0200

    uiddi

--- src/app/utils/config.js
+++ src/app/utils/config.js
@@ -1,3 +1,3 @@
 export const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL;
-const API_V2_BASE_URL = process.env.NEXT_PUBLIC_API_V2_BASE_URL;
+export const API_V2_BASE_URL = process.env.NEXT_PUBLIC_API_V2_BASE_URL;
 
