6bb65e300f41d1eb5378ae54076ae0cfd8e0e0ea
Karimtarek011420 <karimtarek011420@gmail.com>
2025-02-17 04:44:19 +0200

    uiddi

--- src/app/(MainCompoments)/OrderSuccess/[id]/page.jsx
+++ src/app/(MainCompoments)/OrderSuccess/[id]/page.jsx
@@ -2,7 +2,9 @@
 import { API_V2_BASE_URL } from "@/app/utils/config";
 import axios from "axios";
 import React, { use, useEffect, useState } from "react";
-
+import "./ordersucess.css";
+import ordersucess from "@/assets/images/ordersucess.svg";
+import Image from "next/image";
 export default function page({ params: paramsPromise }) {
   const params = use(paramsPromise);
   const { id } = params;
@@ -31,7 +33,23 @@ export default function page({ params: paramsPromise }) {
 
   return (
     <>
-      <div>Page {id}</div>
+      <div className="ordersucess position-relative py-5">
+        <div className="position-absolute country-listbeginall w-100">
+          <ul className="list-unstyled d-flex justify-content-center align-items-center">
+            <li
+              className="country-list-links bg-white mx-lg-2"
+              style={{ color: "var(--primary-color)" }}
+            >
+              <span>تم تأكيد طلبك بنجاح</span>
+            </li>
+          </ul>
+        </div>
+        <div className="container py-5">
+          <div>
+            <Image src={ordersucess} width={99} height={99} alt="ordersucess" />
+          </div>
+        </div>
+      </div>
     </>
   );
 }
