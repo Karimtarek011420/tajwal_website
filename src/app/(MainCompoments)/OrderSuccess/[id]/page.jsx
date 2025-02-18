9d9e33e7df444079c34593d0a3e996a18ad98187
Karimtarek011420 <karimtarek011420@gmail.com>
2025-02-19 00:33:51 +0200

    uiddi

--- src/app/(MainCompoments)/OrderSuccess/[id]/page.jsx
+++ src/app/(MainCompoments)/OrderSuccess/[id]/page.jsx
@@ -26,8 +26,10 @@ function pagesucess({ params: paramsPromise }) {
   const params = use(paramsPromise);
   const { id } = params;
   const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
 
   const apiDetialsOrder = async () => {
+    setLoading(true);
     try {
       const { data } = await axios.get(
         `${API_V2_BASE_URL}/get_my_esims_details/${id}`,
@@ -43,10 +45,14 @@ function pagesucess({ params: paramsPromise }) {
     } catch (error) {
       console.log(error);
     }
+    setLoading(false);
   };
   useEffect(() => {
     apiDetialsOrder();
   }, []);
+  if (loading) {
+    return <p className="text-center py-5">جارٍ تحميل البيانات...</p>;
+  }
 
   return (
     <>
@@ -121,7 +127,9 @@ function pagesucess({ params: paramsPromise }) {
                   <p className="mx-1 my-0">التغطية</p>
                 </div>
                 <div>
-                {data?.esims?.[0] && <p className="my-0">{data.esims[0].country_name}</p>}
+                  {data?.esims?.[0] && (
+                    <p className="my-0">{data.esims[0].country_name}</p>
+                  )}
                 </div>
               </div>
               <div className="d-flex justify-content-between align-items-center text-center p-3 rounded-3 m-2 mt-3 bg-white shadow-sm ordersucessdetilas">
@@ -144,7 +152,12 @@ function pagesucess({ params: paramsPromise }) {
                   <p className="mx-1 my-0">الصلاحية</p>
                 </div>
                 <div>
-                {data?.esims?.[0] && <p className="my-0">{data.esims[0].package_day} {data.esims[0].package_day <= 10 ? "أيام" : "يوم"}</p>}
+                  {data?.esims?.[0] && (
+                    <p className="my-0">
+                      {data.esims[0].package_day}{" "}
+                      {data.esims[0].package_day <= 10 ? "أيام" : "يوم"}
+                    </p>
+                  )}
                 </div>
               </div>
             </div>
