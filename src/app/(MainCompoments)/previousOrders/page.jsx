88606bd73e428481b5ed30cee59f025dd18f38db
Karimtarek011420 <karimtarek011420@gmail.com>
2025-02-19 02:58:50 +0200

    uiddi

--- src/app/(MainCompoments)/previousOrders/page.jsx
+++ src/app/(MainCompoments)/previousOrders/page.jsx
@@ -1,18 +1,15 @@
 "use client";
 import Link from "next/link";
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./previousOrders.css";
 import { usePathname, useRouter } from "next/navigation";
 import { authtoken } from "@/app/_Compontents/Authtoken/Authtoken";
 import { logoutApi } from "@/app/Hookshelp/logout";
+import arrow from "@/assets/images/arrow.svg";
 import axios from "axios";
-import Swal from "sweetalert2";
-import { TailSpin } from "react-loader-spinner";
-import { PhoneInput } from "react-international-phone";
-import "react-international-phone/style.css";
-import { API_BASE_URL } from "@/app/utils/config";
 import withAuth from "@/app/utils/withAuth";
-
+import Image from "next/image";
+import { API_BASE_URL } from "@/app/utils/config";
 function previousOrders() {
   const [user, setUser] = useState(() => {
     try {
@@ -24,6 +21,28 @@ function previousOrders() {
   const { token, settoken } = useContext(authtoken);
   const router = useRouter();
   const pathName = usePathname();
+  const [data, setdata] = useState(null);
+
+  const [error, setError] = useState(null); // تخزين الخطأ
+
+  const apiOrders = async () => {
+    try {
+      const { data } = await axios.get(`${API_BASE_URL}/get_order`, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          Accept: "application/json",
+        },
+      });
+      console.log(data.data);
+      setdata(data.data); // تعيين البيانات في state
+      setError(null);
+    } catch (error) {
+      setError("حدث خطأ أثناء تحميل البيانات. يرجى المحاولة لاحقًا.");
+    }
+  };
+  useEffect(() => {
+    apiOrders();
+  }, []);
 
   const handleLogout = () => {
     if (token) {
@@ -33,13 +52,25 @@ function previousOrders() {
       });
     }
   };
+  if (!data?.length) {
+    return (
+      <div
+        className="error-message text-center py-5"
+        style={{ minHeight: "30vh" }}
+      >
+        <p className="text-danger">
+         لا يوجد طلبات الان
+        </p>
+      </div>
+    );
+  }
 
   return (
     <div className="accountInformation position-relative py-5">
       <div className="position-absolute country-listbeginall w-100">
         <ul className="list-unstyled d-flex justify-content-center align-items-center">
           <li className="country-list-links bg-white mx-2 accountInformationp">
-            معلومات الحساب
+            الطلبات السابقة
           </li>
         </ul>
       </div>
@@ -98,7 +129,54 @@ function previousOrders() {
               </div>
             </div>
           </div>
-          <div className=" col-md-6 offset-2"></div>
+          <div className=" col-md-8">
+            <div className=" container">
+              <div className="d-flex justify-content-center">
+                {error && (
+                  <p
+                    className="text-danger text-center py-5"
+                    style={{ minHeight: "30vh" }}
+                  >
+                    {error}
+                  </p>
+                )}{" "}
+              </div>
+              <div className=" row gy-4">
+                {data?.map((order) => {
+                  return (
+                    <div key={order.id} className="col-md-4">
+                      <div className="bg-white shadow-sm text-center">
+                        <Link href="#">
+                          <div className="d-flex justify-content-between align-items-center p-3">
+                            <div className="country-flag d-flex justify-content-center align-items-center">
+                              <Image
+                                src={order.country.image}
+                                width={60}
+                                height={40}
+                                loading="lazy"
+                                alt="img-country"
+                              />
+                              <div>
+                                <p className="text-black mb-0 ms-lg-4 px-lg-3 countryname">
+                                  {order.id}
+                                </p>
+                              </div>
+                            </div>
+                            <Image
+                              src={arrow}
+                              width={8}
+                              height={15}
+                              alt="arrow"
+                            />
+                          </div>
+                        </Link>
+                      </div>
+                    </div>
+                  );
+                })}
+              </div>
+            </div>
+          </div>
         </div>
       </div>
     </div>
