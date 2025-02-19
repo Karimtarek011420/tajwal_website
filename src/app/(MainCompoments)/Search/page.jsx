c5aa645b92b8165a84dd8c70df9ed0b55d81109c
Karimtarek011420 <karimtarek011420@gmail.com>
2025-02-19 21:59:52 +0200

    uiddi

--- src/app/(MainCompoments)/Search/page.jsx
+++ src/app/(MainCompoments)/Search/page.jsx
@@ -4,6 +4,7 @@ import { useSearchParams } from "next/navigation";
 import axios from "axios";
 import Link from "next/link";
 import Image from "next/image";
+import "./search.css";
 import arrow from "../../../assets/images/arrow.svg";
 import { API_BASE_URL } from "@/app/utils/config";
 
@@ -47,7 +48,26 @@ export default function SearchResults() {
   }, [searchTerm]);
 
   return (
-    <main className="container py-5">
+    <main className="countryheader position-relative  p-5">
+      <div className="position-absolute country-listbeginall w-100">
+        <ul className="list-unstyled d-flex justify-content-center align-items-center">
+          <li
+            className="country-list-links bg-white mx-2"
+            style={{ color: "var(--primary-color)" }}
+          >
+            دولية
+          </li>
+          <Link
+            href="/continents"
+            className="country-list-links text-white mx-2"
+          >
+            قارية
+          </Link>
+          <Link href="/world" className="country-list-links text-white mx-2">
+            عالمية
+          </Link>
+        </ul>
+      </div>
       {/* <span className="text-center text-body-secondary mb-3">
         نتائج البحث عن: {searchTerm}
       </span> */}
@@ -66,37 +86,38 @@ export default function SearchResults() {
           لا توجد نتائج بحث
         </p>
       )}
-
-      <div className="row">
-        {results.map((country) => (
-          <div key={country.country_code} className="col-md-3 mb-4">
-            <div className="bg-white shadow-sm text-center">
-              <Link
-                href={
-                  country.type === "local"
-                    ? `/Countries/${country.country_code}`
-                    : `/continents/${country.country_code}`
-                }
-              >
-                <div className="d-flex justify-content-between align-items-center p-3">
-                  <div className="country-flag d-flex justify-content-center align-items-center">
-                    <Image
-                      src={country.image}
-                      width={60}
-                      height={40}
-                      alt={`علم ${country.title}`}
-                      loading="lazy"
-                    />
-                    <p className="text-black mb-0 ms-lg-4 px-lg-3 countryname">
-                      {country.title}
-                    </p>
+      <div className="px-lg-5">
+        <div className="row gy-4">
+          {results.map((country) => (
+            <div key={country.country_code} className="col-md-3 ">
+              <div className="bg-white shadow-sm text-center">
+                <Link
+                  href={
+                    country.type === "local"
+                      ? `/Countries/${country.country_code}`
+                      : `/continents/${country.country_code}`
+                  }
+                >
+                  <div className="d-flex justify-content-between align-items-center p-3">
+                    <div className="country-flag d-flex justify-content-center align-items-center">
+                      <Image
+                        src={country.image}
+                        width={60}
+                        height={40}
+                        alt={`علم ${country.title}`}
+                        loading="lazy"
+                      />
+                      <p className="text-black mb-0 ms-lg-4 px-lg-3 countryname">
+                        {country.title}
+                      </p>
+                    </div>
+                    <Image src={arrow} width={20} height={20} alt="السهم" />
                   </div>
-                  <Image src={arrow} width={20} height={20} alt="السهم" />
-                </div>
-              </Link>
+                </Link>
+              </div>
             </div>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </main>
   );
