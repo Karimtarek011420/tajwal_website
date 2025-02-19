e5f6e7ca5160aa1ac64ba42f326a252b33d2633f
Karimtarek011420 <karimtarek011420@gmail.com>
2025-02-19 23:59:52 +0200

    uiddi

--- src/app/(MainCompoments)/Orders/[id]/page.jsx
+++ src/app/(MainCompoments)/Orders/[id]/page.jsx
@@ -1,5 +1,108 @@
-import React from "react";
+"use client";
+import Link from "next/link";
+import React, { useContext, useEffect, useState } from "react";
+import "./Orderdetilas.css";
+import { usePathname, useRouter } from "next/navigation";
+import { authtoken } from "@/app/_Compontents/Authtoken/Authtoken";
+import { logoutApi } from "@/app/Hookshelp/logout";
+import arrow from "@/assets/images/arrow.svg";
+import axios from "axios";
+import withAuth from "@/app/utils/withAuth";
+import Image from "next/image";
+import { API_BASE_URL } from "@/app/utils/config";
+function Ordersdetails() {
+  const [user, setUser] = useState(() => {
+    try {
+      return JSON.parse(localStorage.getItem("user")) || {};
+    } catch {
+      return {};
+    }
+  });
+  const { token, settoken } = useContext(authtoken);
+  const router = useRouter();
+  const pathName = usePathname();
+  const [data, setdata] = useState(null);
+  const [error, setError] = useState(null); // تخزين الخطأ
+  const [loading, setLoading] = useState(false);
 
-export default function page() {
-  return <div>page</div>;
+ 
+
+  const handleLogout = () => {
+    if (token) {
+      logoutApi(token, settoken);
+      setTimeout(() => {
+        router.push("/");
+      });
+    }
+  };
+
+  return (
+    <div className="Ordersdetails position-relative py-5">
+      <div className="position-absolute country-listbeginall w-100">
+        <ul className="list-unstyled d-flex justify-content-center align-items-center">
+          <li className="country-list-links bg-white mx-2 accountInformationp">
+            <span className="p-5"> الطلبات السابقة</span>
+          </li>
+        </ul>
+      </div>
+      <div className="container py-5">
+        <div className="row gy-4">
+          <div className="col-md-3 offset-1">
+            <div className="cardinfo bg-info py-3 bg-white shadow-sm rounded-4 ps-5">
+              <div className="px-3">
+                <h6>{user?.first_name}</h6>
+                <span dir="ltr">{user?.phone_number}</span>
+              </div>
+              <hr
+                style={{ borderColor: "gray", margin: "10px", width: "100%" }}
+              />
+              <div>
+                <ul className="list-unstyled px-3">
+                  <Link href="/accountInformation">
+                    <li
+                      className={
+                        pathName === "/accountInformation" ? "active" : ""
+                      }
+                    >
+                      معلومات الحساب
+                    </li>
+                  </Link>
+                  <Link href="/Orders">
+                    <li className={pathName === "/Orders" ? "active" : ""}>
+                      الطلبات السابقة
+                    </li>
+                  </Link>
+                  <Link href="/helpCenter">
+                    <li className={pathName === "/helpCenter" ? "active" : ""}>
+                      مركز المساعدة
+                    </li>
+                  </Link>
+                  <Link href="/Customerservice">
+                    <li
+                      className={
+                        pathName === "/Customerservice" ? "active" : ""
+                      }
+                    >
+                      خدمة العملاء
+                    </li>
+                  </Link>
+                  <Link href={"#"} onClick={handleLogout}>
+                    <li
+                      className="accountInformationlogout"
+                      aria-label="Logout"
+                    >
+                      تسجيل الخروج
+                    </li>
+                  </Link>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
+
+export default withAuth(Ordersdetails);
+
