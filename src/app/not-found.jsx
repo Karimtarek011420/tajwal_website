05ed99810423c58e38be9c479097f4f154ec7268
Karimtarek011420 <karimtarek011420@gmail.com>
2025-02-20 02:32:47 +0200

    uiddi

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -3,13 +3,14 @@ import Header from "./_Compontents/Header/Header";
 import Footer from "./_Compontents/Footer/Footer";
 import footerimage from "@/assets/images/5052790.webp";
 import Image from "next/image";
+// import './_Compontents/Footer/footer.css'
 
 const NotFoundPage = () => {
   return (
     <>
       <Header />
       <div className=" d-flex justify-content-center align-items-center py-2">
-        <Image src={footerimage} alt="iconcountry" className=" text-white" />
+        <Image src={footerimage} alt="iconcountry" className=" text-white imgnotfound " />
       </div>
       <Footer />
     </>
