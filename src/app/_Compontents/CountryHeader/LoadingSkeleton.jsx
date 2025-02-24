9b7bc7ee5b6f80ba7d4fe2ec4483eb78c3d05dc3
Karimtarek011420 <karimtarek011420@gmail.com>
2025-02-24 03:43:29 +0200

    uiddi

--- src/app/_Compontents/CountryHeader/LoadingSkeleton.jsx
+++ src/app/_Compontents/CountryHeader/LoadingSkeleton.jsx
@@ -5,20 +5,25 @@ import "react-loading-skeleton/dist/skeleton.css";
 export default function LoadingSkeleton() {
   return (
     <div className="px-5 py-5">
-      <div  className="row row-cols-lg-5 row-cols-md-3 row-cols-sm-2 row-cols-1 gy-4 px-lg-5">
-        {Array.from({ length: 12 }).map((_, index) => (
-          <div key={index} className="col">
-            <div className="bg-white shadow-sm text-center">
-              <div className="d-flex justify-content-between align-items-center p-3">
-                <div className="country-flag d-flex justify-content-center align-items-center">
-                  <Skeleton w height={40} />
-                  <Skeleton height={80} className="ms-lg-4 px-lg-3" />
+      <div className="row row-cols-lg-5 row-cols-md-3 row-cols-sm-2 row-cols-1 gy-4 px-lg-5">
+        {Array.from({ length: 10 }).map(
+          (
+            _,
+            index // عدد العناصر يمكن تعديله حسب الحاجة
+          ) => (
+            <div key={index} className="col">
+              <div className="bg-white shadow-sm text-center rounded-3 p-3">
+                <div className="d-flex justify-content-between align-items-center">
+                  <div className="d-flex align-items-center">
+                    <Skeleton width={45} height={30} />
+                    <Skeleton width={80} height={20} className="ms-lg-3 ms-2" />
+                  </div>
+                  <Skeleton width={15} height={25} />
                 </div>
-                <Skeleton height={20} />
               </div>
             </div>
-          </div>
-        ))}
+          )
+        )}
       </div>
     </div>
   );
