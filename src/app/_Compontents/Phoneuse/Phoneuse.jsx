1cdee2ef436bb075579fe85a2fd2b195d680325f
Karimtarek011420 <karimtarek011420@gmail.com>
2025-02-25 03:21:26 +0200

    uiddi

--- src/app/_Compontents/Phoneuse/Phoneuse.jsx
+++ src/app/_Compontents/Phoneuse/Phoneuse.jsx
@@ -13,19 +13,19 @@ export default function Phoneuse() {
         <h4 className=" headphone py-5 m-0">كيف تستخدم “تجوال”</h4>
         <div className="row gy-5 gx-2 ">
           <div className="col-md-3  text-center">
-            <Image src={layer1} width={200} height={400} alt="phone-download" />
+            <Image src={layer1} width={300} height={400} alt="phone-download" />
             <h4 className="  py-3 phoneusepra"> حمل التطبيق</h4>
           </div>
           <div className="col-md-3 text-center">
-            <Image src={layer2} width={200} height={400} alt="phone-download" />
+            <Image src={layer2} width={300} height={400} alt="phone-download" />
             <h4 className="  py-3 phoneusepra"> اختر الوجهة والباقة </h4>
           </div>
           <div className="col-md-3 text-center">
-            <Image src={layer3} width={200} height={400} alt="phone-download" />
+            <Image src={layer3} width={300} height={400} alt="phone-download" />
             <h4 className="  py-3 phoneusepra"> ثبت الشريحة</h4>
           </div>
           <div className="col-md-3 text-center">
-            <Image src={layer4} width={200} height={400} alt="phone-download" />
+            <Image src={layer4} width={300} height={400} alt="phone-download" />
             <h4 className="  py-3 phoneusepra"> استمتع بالباقة </h4>
           </div>
         </div>
