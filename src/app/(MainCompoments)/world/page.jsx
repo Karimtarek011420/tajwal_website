e471c5bdd988cdeb3689e3d41b4aa56eaccada27
Karimtarek011420 <karimtarek011420@gmail.com>
2025-02-28 07:29:39 +0200

    uiddi

--- src/app/(MainCompoments)/world/page.jsx
+++ src/app/(MainCompoments)/world/page.jsx
@@ -21,6 +21,7 @@ import { usePackage } from "@/app/_Compontents/PackageContext/PackageContext";
 import { Modal, Box, Button, Typography } from "@mui/material";
 import { API_BASE_URL } from "@/app/utils/config";
 import { authtoken } from "@/app/_Compontents/Authtoken/Authtoken";
+import toast from "react-hot-toast";
 export default function DetailsCountry() {
   const router = useRouter();
   const [data, setData] = useState(null);
