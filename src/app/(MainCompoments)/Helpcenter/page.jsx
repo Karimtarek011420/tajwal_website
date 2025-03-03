18bd8ec0652052ccab9254b53b49d2fb43dbd5c7
Karimtarek011420 <karimedris398@gmail.com>
2025-03-03 14:08:16 +0200

    sec commit

--- src/app/(MainCompoments)/Helpcenter/page.jsx
+++ src/app/(MainCompoments)/Helpcenter/page.jsx
@@ -1,7 +1,22 @@
-import React from 'react'
-
-export default function page() {
+"use client";
+import React, { useEffect, useState } from "react";
+import "./Helpcenter.css";
+export default function OffersPage() {
   return (
-    <div>page</div>
-  )
+    <div className="offers position-relative py-5">
+      <div className="position-absolute country-listbeginall w-100">
+        <ul className="list-unstyled d-flex justify-content-center align-items-center">
+          <li
+            className="country-list-links  mx-lg-2"
+            style={{
+              color: "var(--primary-color)",
+              backgroundColor: "var(--background)",
+            }}
+          >
+            <span className="p-5">العروض</span>
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
 }
