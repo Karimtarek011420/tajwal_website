fc785a6f94103978a500da94fb6fc9c98ee3cd2e
Karimtarek011420 <karimedris398@gmail.com>
2025-03-03 16:21:47 +0200

    sec commit

--- src/app/(MainCompoments)/Helpcenter/page.jsx
+++ src/app/(MainCompoments)/Helpcenter/page.jsx
@@ -1,6 +1,9 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import "./Helpcenter.css";
+import Image from "next/image";
+import arrow from "@/assets/images/arrow.svg";
+
 export default function OffersPage() {
   return (
     <div className="Helpcenter position-relative py-5">
@@ -27,6 +30,19 @@ export default function OffersPage() {
           </button>
           <input type="text" placeholder="اكتب نبذة عن المشكلة التي تواجهها" />
           <p className="Helpcenterpsearch">ابحث</p>
+        </div>
+        <div className=" row gy-4 py-5 justify-content-center align-items-center">
+          <div className="col-md--3">
+            <div className=" bg-white shadow-sm rounded-3">
+              <Image
+                src={arrow}
+                alt="Helpcentertajwal"
+                width={26}
+                height={35}
+                priority
+              />
+            </div>
+          </div>
         </div>
       </div>
     </div>
