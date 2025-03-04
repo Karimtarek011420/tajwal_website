d7ff23fd0d1505b46b90a0056a68f14c375712bd
Karimtarek011420 <karimedris398@gmail.com>
2025-03-04 14:31:31 +0200

    sec commit

--- src/app/(MainCompoments)/Helpcenter/page.jsx
+++ src/app/(MainCompoments)/Helpcenter/page.jsx
@@ -6,7 +6,10 @@ import Helpcentertwal from "@/assets/images/footerHero.svg";
 import Isolation_Mode from "@/assets/images/Isolation_Mode.svg";
 import eorrhelp from "@/assets/images/eorrhelp.svg";
 
-export default function OffersPage() {
+export default function HelpcenterPage() {
+  const faqs = Array(8).fill(
+    "لوريم إيبسوم هو ببساطة نص شكلي (بمعنى أن الغاية هي الشكل وليس المحتوى)، ويستخدم في صناعات المطابع ودور النشر. كان لوريم إيبسوم ولايزال المعيار للنص الشكلي."
+  );
   return (
     <div className="Helpcenter position-relative py-5">
       <div className="position-absolute country-listbeginall w-100">
@@ -33,7 +36,7 @@ export default function OffersPage() {
           <input type="text" placeholder="اكتب نبذة عن المشكلة التي تواجهها" />
           <p className="Helpcenterpsearch">ابحث</p>
         </div>
-        <div className=" row gy-4 py-5 justify-content-center align-items-center px-lg-5">
+        <div className=" row gy-3 py-5 justify-content-center align-items-center px-lg-5">
           <div className="col-md-4">
             <div className=" bg-white shadow-sm rounded-3  text-center px-5 py-3">
               <Image
@@ -76,6 +79,21 @@ export default function OffersPage() {
                 قد تواجهك بعض المشاكل في اي من مراحل تفعيل او استخدام الشريحة،
                 هنا تجد الحلول.
               </p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <p className="me-lg-5 Helpcenterpquestion ">الاسئلة الأكثر شيوعاً</p>
+          <div className="   bg-white shadow-sm rounded-3 py-4  px-lg-4 px-3 mx-lg-5">
+            <div className="row">
+              {faqs.map((faq, index) => (
+                <div key={index} className="col-md-6 mb-3">
+                  <li className="list-group-item border-0 d-flex align-items-start text-end">
+                    <i className="fa-solid fa-circle m-2 " style={{fontSize:'9px' , color:'var(--auth-color)'}}></i>
+                    <span className="Helpcenterpspan">{faq}</span>
+                  </li>
+                </div>
+              ))}
             </div>
           </div>
         </div>
