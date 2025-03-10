c39d5c2abc7be23f3e69a662c952e4a9f1a0c2ce
Karimtarek011420 <karimedris398@gmail.com>
2025-03-10 11:43:32 +0200

    sec commit

--- src/app/(MainCompoments)/Offers/loading.jsx
+++ src/app/(MainCompoments)/Offers/loading.jsx
@@ -6,12 +6,12 @@ export default function LoadingOffers() {
   return (
     <div className=" container  py-5">
       <div className=" row gy-4">
-        {Array.from({ length: 6 }).map((_, index) => (
+        {Array.from({ length: 10 }).map((_, index) => (
           <div key={index} className="col-md-4">
-            <div className="offer rounded-5 shadow-sm d-flex justify-content-center  align-items-center">
+            <div className="offer rounded-3 shadow-sm d-flex justify-content-center  align-items-center">
               <Skeleton height={200} />
               <div className="pt-5 pb-3 d-flex justify-content-center align-items-center">
-                <Skeleton width={120} height={40} />
+                <Skeleton width={150} height={40} />
               </div>
             </div>
           </div>
