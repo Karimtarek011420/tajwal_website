45174fc0462a9dcdc681df5253cc836634a726d0
Karimtarek011420 <karimedris398@gmail.com>
2025-03-10 11:38:54 +0200

    sec commit

--- src/app/(MainCompoments)/Offerdetials/page.jsx
+++ src/app/(MainCompoments)/Offerdetials/page.jsx
@@ -35,7 +35,7 @@ const OfferDetails = () => {
             left: 0,
             width: "100vw",
             height: "100vh",
-            backgroundColor: "rgba(255, 255, 255, 0.8)",
+            backgroundColor: " var(--background)",
             zIndex: 1000,
           }}
         >
