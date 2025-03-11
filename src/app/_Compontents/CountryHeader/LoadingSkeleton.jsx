daeee9b30e99f05cc76dc1302a7a2a041ceaef9c
Karimtarek011420 <karimedris398@gmail.com>
2025-03-11 12:48:29 +0200

    sec commit

--- src/app/_Compontents/CountryHeader/LoadingSkeleton.jsx
+++ src/app/_Compontents/CountryHeader/LoadingSkeleton.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import Skeleton from "react-loading-skeleton";
 import "react-loading-skeleton/dist/skeleton.css";
-
 export default function LoadingSkeleton() {
   return (
     <div className="px-5 py-5">
