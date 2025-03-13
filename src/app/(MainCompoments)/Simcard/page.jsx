e3ac19ed42c8892d6422dc5572dabae6960c20e7
Karimtarek011420 <karimedris398@gmail.com>
2025-03-13 12:41:30 +0200

    sec commit

--- src/app/(MainCompoments)/Simcard/page.jsx
+++ src/app/(MainCompoments)/Simcard/page.jsx
@@ -1,10 +1,11 @@
 "use client";
 import React, { useEffect, useState } from "react";
 import "./Simcard.css";
+import Link from "next/link";
 export default function SimcardPage() {
   return (
     <div className="Simcard  position-relative py-5">
-      <div className="position-absolute country-listbeginall w-100">
+      <div className="position-absolute country-listbeginall w-100 ">
         <ul className="list-unstyled d-flex justify-content-center align-items-center">
           <li
             className="country-list-links  mx-lg-2"
@@ -17,9 +18,10 @@ export default function SimcardPage() {
           </li>
         </ul>
       </div>
-      <div className=" container py-5 bg-white shadow-sm rounded-3 position-relative">
-        <div className=" position-absolute">
-          <button> رمز QR</button>
+      <div className=" container py-5 bg-white shadow-sm rounded-3 position-relative mt-5 ">
+        <div className="position-absolute Simcarchoose  d-flex justify-content-center align-items-center">
+          <button className="Simcard-list-links mx-2">رمز QR</button>
+          <button className="Simcard-list-links mx-2 ">يدوي</button>
         </div>
       </div>
     </div>
