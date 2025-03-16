35a423efc6233974c361db4283827b35e6603d5d
Karimtarek011420 <karimedris398@gmail.com>
2025-03-16 17:46:13 +0200

    sec commit

--- src/app/(MainCompoments)/Orders/[id]/page.jsx
+++ src/app/(MainCompoments)/Orders/[id]/page.jsx
@@ -322,7 +322,18 @@ function Ordersdetails() {
                       </p>
                     </div>
                     <div className=" py-2 px-4 wayslink rounded-3 mx-5">
-                      <Link href="/Simcard">
+                      {console.log(data.esims)}
+                      <Link
+                        href={{
+                          pathname: "/Simcard",
+                          query: {
+                            lpa: data?.esims[0]?.esim_installation_data?.lpa,
+                            matching_id:
+                              data?.esims[0]?.esim_installation_data?.matching_id,
+                            qrcode: data?.esims[0]?.esim_installation_data?.qrcode,
+                          },
+                        }}
+                      >
                         <div className="d-flex justify-content-between align-items-center ">
                           <div className="country-flag d-flex justify-content-center align-items-center p-1">
                             <Image
