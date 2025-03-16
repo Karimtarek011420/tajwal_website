2d111a4695cfac78d895a7edc9a3afc386581728
Karimtarek011420 <karimedris398@gmail.com>
2025-03-16 11:53:49 +0200

    sec commit

--- src/app/(MainCompoments)/Search/page.jsx
+++ src/app/(MainCompoments)/Search/page.jsx
@@ -87,7 +87,7 @@ export default function SearchResults() {
       )}
       <div className="px-3 px-md-4 px-lg-5">
         <div
-          className="row row-cols-xl-5 row-cols-lg-3 row-cols-md-3 row-cols-sm-2 row-cols-1 gy-3"
+          className="row row-cols-xl-5 row-cols-lg-3 row-cols-md-3 row-cols-sm-2 row-cols-1 gy-4"
           
         >
           {results.map((country) => (
